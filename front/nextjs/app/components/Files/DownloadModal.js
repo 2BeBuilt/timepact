f03e70499e0dde6def9f993c192b3bf9b7640050
Andrey Gruzdev <agruzdev1@gmail.com>
2023-03-21 14:27:31 +0100

    add basic features

---
+++ front/nextjs/app/components/Files/DownloadModal.js
@@ -0,0 +1,48 @@
+'use client'
+
+import { useState } from 'react'
+import { TextInput, Label, Modal, Button } from 'flowbite-react'
+
+export default function DownloadModal({ handleClick }) {
+  const [visible, setVisible] = useState(false)
+  const [cid, setCid] = useState(null)
+  const onCidChange = (e) => {
+    setCid(e.target.value)
+  }
+  return (
+    <>
+      <Button onClick={() => setVisible(true)}>Download</Button>
+      <Modal
+        show={visible}
+        size="md"
+        popup={true}
+        onClose={() => setVisible(false)}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <TextInput
+            id="cidInput"
+            placeholder="Enter CID"
+            required={true}
+            value={cid}
+            onChange={onCidChange}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            disabled={!cid}
+            onClick={() => {
+              window.open(
+                `http://localhost:3010/api/ipfs/retrieve?cid=${cid}`,
+                '_blank'
+              )
+              setVisible(false)
+            }}
+          >
+            Ok
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
---
+++ front/nextjs/app/components/Files/UploadModal.js
@@ -0,0 +1,43 @@
+'use client'
+
+import { useState } from 'react'
+import { FileInput, Label, Modal, Button } from 'flowbite-react'
+
+export default function UploadModal({ handleClick }) {
+  const [visible, setVisible] = useState(false)
+  const [file, selectFile] = useState(null)
+  const handleFileSelected = (e) => {
+    const files = Array.from(e.target.files)
+    selectFile(files[0])
+  }
+  return (
+    <>
+      <Button onClick={() => setVisible(true)}>Upload</Button>
+      <Modal
+        show={visible}
+        size="md"
+        popup={true}
+        onClose={() => setVisible(false)}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div id="fileUpload">
+            <div className="mb-2 block">
+              <Label htmlFor="file" value="Upload file" />
+            </div>
+            <FileInput
+              onChange={handleFileSelected}
+              id="file"
+              helperText="File selected"
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button disabled={!file} onClick={() => handleClick(file)}>
+            Ok
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
