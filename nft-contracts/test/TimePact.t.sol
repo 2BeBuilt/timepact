34ef6d1e2b8dcda2e3c2908507f89178c04e5132
ztsalexey <zthalexey@gmail.com>
2023-03-22 10:55:27 -0500

    1.0

--- nft-contracts/test/TimePact.t.sol
+++ nft-contracts/test/TimePact.t.sol
@@ -4,14 +4,23 @@ pragma solidity ^0.8.17;
 import "lib/forge-std/lib/ds-test/src/test.sol";
 import "contracts/TimePact.sol";
 import "node_modules/@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "contracts/basic-deal-client/DealClient.sol";
+import {MarketTypes} from "@zondax/filecoin-solidity/contracts/v0.8/types/MarketTypes.sol";
+import {serializeDealProposal, deserializeDealProposal} from "contracts/basic-deal-client/Types.sol";
 
 contract TimePactTest is DSTest, ERC721Holder {
     TimePact public timePact;
+    DealClient public client;
+    //MockMarket public relay;
+    bytes testCID;
+    bytes testShortCID;
+    bytes testProvider;
+    bytes testOtherProvider;
 
     function setUp() public {
         timePact = new TimePact();
         client = timePact.dealsClient();
-        relay = new MockMarket();
+        //relay = new MockMarket();
         testCID = hex"000181E2039220206B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B";
         testShortCID = hex"42";
         testProvider = hex"0066";
@@ -60,25 +69,25 @@ contract TimePactTest is DSTest, ERC721Holder {
             })
         });
 
-        IDeal.DealRequest memory idRequest = IDeal.DealRequest({
-            piece_cid: testCID,
-            piece_size: 2048,
-            verified_deal: false,
-            label: "",
-            start_epoch: 0,
-            end_epoch: 0,
-            storage_price_per_epoch: 0,
-            provider_collateral: 0,
-            client_collateral: 0,
-            extra_params_version: 0,
-            extra_params: IDeal.ExtraParamsV1({
-                location_ref: "",
-                car_size: 0,
-                skip_ipni_announce: false,
-                remove_unsealed_copy: false
-            })
-        });
-        return request;
+        // IDeal.DealRequest memory idRequest = IDeal.DealRequest({
+        //     piece_cid: testCID,
+        //     piece_size: 2048,
+        //     verified_deal: false,
+        //     label: "",
+        //     start_epoch: 0,
+        //     end_epoch: 0,
+        //     storage_price_per_epoch: 0,
+        //     provider_collateral: 0,
+        //     client_collateral: 0,
+        //     extra_params_version: 0,
+        //     extra_params: IDeal.ExtraParamsV1({
+        //         location_ref: "",
+        //         car_size: 0,
+        //         skip_ipni_announce: false,
+        //         remove_unsealed_copy: false
+        //     })
+        // });
+        // return request;
     }
 
     function testMakeDealProposalWithTimePact() public {
