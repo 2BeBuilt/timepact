d59405e1153afc1143fc7eb18eee66936bd8d298
Andrey Gruzdev <agruzdev1@gmail.com>
2023-03-23 14:58:38 +0100

    init next stable

---
+++ front/next12/pages/_app.js
@@ -0,0 +1,39 @@
+import '@/styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { filecoinHyperspace, filecoin } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import Navbar from '../components/Navbar'
+
+const { chains, provider } = configureChains(
+  [filecoin, filecoinHyperspace],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'TimePact',
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ChakraProvider>
+          <Navbar />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
---
+++ front/next12/pages/pacts/index.js
@@ -0,0 +1,12 @@
+import Error from '@/components/Alerts/Error'
+import { Button } from '@chakra-ui/react'
+import { useState } from 'react'
+
+export default function Pacts() {
+  const [error, setError] = useState(null)
+  return (
+    <>
+      <main></main>
+    </>
+  )
+}
