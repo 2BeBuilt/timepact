e4dae6a7d905f747a4e62d591d92626da4ce8346
Andrey Gruzdev <agruzdev1@gmail.com>
2023-03-24 14:34:57 +0100

    fix security-key updating on remove

--- front/next12/pages/api/ipfs/retrieve.js
+++ front/next12/pages/api/ipfs/retrieve.js
@@ -1,11 +1,24 @@
-import { unpackToFs } from 'ipfs-car/unpack/fs'
+import axios from 'axios'
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    await unpackToFs({
-      input: `temp/temp.car`,
-      output: `temp/`,
-    })
+    console.log(req.query.cid)
+    axios
+      .get(
+        `http://express:${process.env.EXPRESS_PORT}/api/ipfs/retrieve?cid=${req.query.cid}`
+      )
+      .then(function (response) {
+        // handle success
+        console.log(response)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error)
+      })
+      .finally(function () {
+        // always executed
+      })
+
     res.status(200).json('ok')
   }
 }
