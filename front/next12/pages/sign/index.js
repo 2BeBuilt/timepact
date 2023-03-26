c2760fe28918ecd015bf03533d2dd8c57ac679c3
Andrey Gruzdev <agruzdev1@gmail.com>
2023-03-26 11:00:59 +0200

    add alert

--- front/next12/pages/sign/index.js
+++ front/next12/pages/sign/index.js
@@ -3,20 +3,32 @@ import { useAccount, useNetwork } from 'wagmi'
 import PageAlerts from '@/components/Alerts/PageAlerts'
 import PageHead from '@/components/PageHead'
 import Countdown from 'react-countdown'
+import AlertContainer from '@/components/Alerts/AlertContainer'
+import DefaultAlert from '@/components/Alerts/DefaultAlert'
 
 export default function Sign() {
   const { address, isDisconnected } = useAccount()
   const { chain } = useNetwork()
+  chain && console.log(chain.id)
   return (
     <main>
       <PageHead title="Signing pact" />
-      {/*TODO wrong chain handler*/}
-      {!isDisconnected && (
-        <>
-          <SignPact address={address} />
-        </>
-      )}
-      <PageAlerts chain={chain} isDisconnected={isDisconnected} />
+      {!isDisconnected &&
+        (chain && chain.id === 3141 ? (
+          <>
+            <SignPact address={address} />
+          </>
+        ) : (
+          <AlertContainer>
+            <DefaultAlert
+              isOpen={true}
+              status="warning"
+              title="Switch to filecoin"
+              description="Pacts signing are supported only on filecoin right now"
+            />
+          </AlertContainer>
+        ))}
+      {/* <PageAlerts chain={chain} isDisconnected={isDisconnected} /> */}
     </main>
   )
 }
